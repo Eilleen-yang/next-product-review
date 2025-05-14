eff8b06ba8f5574854ca972083a1cab3a0ae4c2a
양아름 <yangareum0130@gmail.com>
2025-05-14 14:32:39 +0900

    feat: 최적화 단계

--- src/pages/_components/ProductCard.jsx
+++ src/pages/_components/ProductCard.jsx
@@ -1,21 +1,23 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function ProductCard({ product }) {
   return (
-    <Link
-      href={`/products/${product.id}`}
-      className="p-4 border rounded shadow hover:shadow-lg transition"
-    >
-      <img
-        src={product.image}
-        alt={product.name}
-        className="mb-2 w-full h-60 rounded object-cover"
-      />
-      <h3 className="text-lg font-semibold"> {product.name}</h3>
-      <p className="text-sm text-gray-600">{product.description}</p>
-      <p className="mt-2 font-bold text-blue-600">
-        {product.price.toLocaleString()}원
-      </p>
-    </Link>
+    <div className="p-4 border rounded shadow hover:shadow-lg transition">
+      <Link href={`/products/${product.id}`}>
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={400}
+          height={300}
+          className="mb-2 w-full h-60 rounded object-cover"
+        />
+        <h3 className="text-lg font-semibold"> {product.name}</h3>
+        <p className="text-sm text-gray-600">{product.description}</p>
+        <p className="mt-2 font-bold text-blue-600">
+          {product.price.toLocaleString()}원
+        </p>
+      </Link>
+    </div>
   );
 }
